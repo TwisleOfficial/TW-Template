e095ad8ea32a62bc085a5e8be54d38f70a87ea96
Twisle <102629991+TwisleOfficial@users.noreply.github.com>
2025-02-14 09:24:17 -0500

    Multi Framework Setup
    Centralized functions to pick what solution to use for commonly used functions amongst frameworks. For example;
    AddMoney
    RemoveMoney
    Notifications
    ProgressBars

--- client/main.lua
+++ client/main.lua
@@ -1,6 +1,82 @@
-local QBCore
-
 if Config.Framework == 'qb' then QBCore = exports['qb-core']:GetCoreObject() end
+
+---@param duration number # length of progress
+---@param label string # progress text
+---@param anim table # {dict, clip}
+local function DoProgress(duration, label, anim)
+    if Config.Progress == 'qb' then
+        QBCore.Functions.Progressbar(label, label, duration, false, true, {
+                disableMovement = true,
+                disableCarMovement = false,
+                disableMouse = false,
+                disableCombat = true,
+            },
+            {
+                animDict = anim[1],
+                anim = anim[2],
+            }, {}, {}, function()
+                return true
+            end, function()
+                return false
+            end)
+    elseif Config.Progress == 'ox-normal' then
+        if lib.progressBar({
+                duration = duration,
+                label = label,
+                useWhileDead = false,
+                canCancel = true,
+                disable = {
+                    move = true,
+                },
+                anim = {
+                    dict = anim[1],
+                    cip = anim[2],
+                }
+            })
+        then
+            return true
+        else
+            return false
+        end
+    else
+        if lib.progressCircle({
+                duration = duration,
+                label = label,
+                position = Config.OxCirclePosition,
+                useWhileDead = false,
+                canCancel = true,
+                disable = {
+                    move = true,
+                },
+                anim = {
+                    dict = anim[1],
+                    clip = anim[2],
+                }
+            })
+        then
+            return true
+        else
+            return false
+        end
+    end
+end
+
+---@param duration number # Length of noti
+---@param title string # noti title can be empty string for qb
+---@param desc string # noti desc
+---@param type string # success or error
+local function DoNotify(duration, title, desc, type)
+    if Config.Notification == 'qb' then
+        QBCore.Functions.Notify(title .. ": " .. desc, type, duration)
+    else
+        lib.notify({
+            title = title,
+            description = desc,
+            type = type,
+            duration = duration,
+        })
+    end
+end
 
 Citizen.CreateThread(function() -- Start Thread (Non Loop)
 
@@ -17,15 +93,10 @@ RegisterNetEvent('nameOFscript:client:nameOFwhathappens', function ()
 end)
 
 AddEventHandler('onResourceStart', function(resourceName)
-    if (GetCurrentResourceName() ~= resourceName) then
-        return
-    end
+    if (GetCurrentResourceName() ~= resourceName) then return end
 
 end)
 
 AddEventHandler('onResourceStop', function(resourceName)
-    if (GetCurrentResourceName() ~= resourceName) then
-      return
-    end
-
+    if (GetCurrentResourceName() ~= resourceName) then return end
 end)
--- server/main.lua
+++ server/main.lua
@@ -1,6 +1,94 @@
-local QBCore
-
 if Config.Framework == 'qb' then QBCore = exports['qb-core']:GetCoreObject() end
+
+---@param src number # player source
+---@param duration number # Length of noti
+---@param title string # noti title can be empty string for qb
+---@param desc string # noti desc
+---@param type string # success or error
+local function DoNotify(src, duration, title, desc, type)
+  if Config.Notification == 'ox' then
+    TriggerClientEvent('ox_lib:notify', src, {
+      title = title,
+      description = desc,
+      type = type,
+      duration = duration,
+      showDuration = true,
+    })
+  else
+    TriggerClientEvent('QBCore:Notify', src, title .. ' ' .. desc, type, duration)
+  end
+end
+
+---@param src number # player source
+---@param amount number # amount to take from player
+local function takeMoney(src, amount)
+  if Config.Framework == 'qbx' then
+    if qbx:GetMoney(src, 'cash') >= amount then
+      qbx:RemoveMoney(src, 'cash', amount, 'Renting Farm Vehicle')
+      return true
+    elseif qbx:GetMoney(src, 'bank') >= amount then
+      qbx:RemoveMoney(src, 'bank', amount, 'Renting Farm Vehicle')
+      return true
+    else
+      return false
+    end
+  end
+  if Config.Framework == 'qb' then
+    local plr = qb.Functions.GetPlayer(src)
+    if plr.Functions.GetMoney('cash') > amount then
+      plr.Functions.RemoveMoney('cash', amount)
+      return true
+    elseif plr.Functions.GetMoney('bank') >= amount then
+      plr.Functions.RemoveMoney('bank', amount)
+      return true
+    else
+      return false
+    end
+  end
+end
+
+---@param src number # player source
+---@param amount number # amount to take from player
+---@param account string # bank or cash
+---@param reason string # reason for change
+local function takeMoney(src, amount, account, reason)
+  if Config.Framework == 'qbx' then
+    qbx:RemoveMoney(src, account, amount, reason)
+  else
+    local plr = qb.Functions.GetPlayer(src)
+    plr.Functions.AddMoney(account, amount, reason)
+  end
+end
+
+---@param src number # player source
+---@param item string # item name
+---@param amount number # amount to take from player
+local function removeItem(src, item, amount)
+  if Config.Inventory == 'ox' then
+    exports.ox_inventory:RemoveItem(src, item, amount)
+  else
+    exports['qb-inventory']:RemoveItem(src, item, amount)
+  end
+end
+
+---@param src number # player source
+---@param item string # item name
+---@param amount number # amount to take from player
+local function addItem(src, item, amount)
+  if Config.Inventory == 'ox' then
+    if exports.ox_inventory:CanCarryItem(src, item, amount) then
+      exports.ox_inventory:AddItem(src, item, amount)
+    else
+      DoNotify(src, 5000, 'Inventory: ', 'You cant carry that!', 'error')
+    end
+  else
+    if exports['qb-inventory']:CanAddItem(src, item, amount) then
+      exports['qb-inventory']:AddItem(src, item, amount)
+    else
+      DoNotify(src, 5000, 'Inventory: ', 'You cant carry that!', 'error')
+    end
+  end
+end
 
 lib.callback.register('nameOFscript:server:nameOFwhathappens', function(source)
   return true
